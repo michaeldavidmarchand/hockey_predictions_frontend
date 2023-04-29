5e135f9df83a692a83ecdc3c603d8b4121d9f477
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-04-29 15:46:46 -0600

    adds more assertions for the user service test

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -13,6 +13,12 @@ describe 'user service' do
       }
 
       posted_user = UserService.new.post_user(body)
+      expect(posted_user).to be_a Hash
+      expect(posted_user).to have_key :data
+      expect(posted_user[:data]).to be_a Hash
+      expect(posted_user[:data]).to have_key :id
+      expect(posted_user[:data]).to have_key :type
+      expect(posted_user[:data][:type]).to eq('user')
     end
   end
 end

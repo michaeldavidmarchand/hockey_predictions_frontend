747e6f7206eee821a082bcf831e0b8b350da1d75
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-06 09:34:08 -0600

    Update test config to use RackSessionAccess

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #
+  # code i added to allow for a session to be set; this is used with rack_session_access gem
+  config.middleware.use RackSessionAccess::Middleware
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that

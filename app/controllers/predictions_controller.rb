50d732125309af7b2ef7944fa9a3e1f64c3dea6d
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-08 14:13:37 -0600

    Adds edit update and destroy actions to predictions#controller

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -7,14 +7,28 @@ class PredictionsController < ApplicationController
     @game_stats = HockeyDataFacade.new.single_game_stats(params[:id])
   end
 
+  def edit
+    @game_stats = HockeyDataFacade.new.single_game_stats(params[:gamePk])
+  end
+
   def create
     HockeyDataFacade.new.add_user_prediction(prediction_params)
     redirect_to user_path(prediction_params[:user_id])
   end
 
+  def update
+    UserFacade.new.update_user_prediction(prediction_params)
+    redirect_to user_path(prediction_params[:user_id])
+  end
+
+  def destroy
+    UserFacade.new.delete_prediction(params[:user_id], params[:id])
+    redirect_to user_path(params[:user_id])
+  end
+
   private
 
   def prediction_params
-    params.permit(:expected_winner, :user_id, :gamePk)
+    params.permit(:expected_winner, :user_id, :gamePk, :id)
   end
 end

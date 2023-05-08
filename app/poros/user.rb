56ab038779e08daf8e51d14417074a67a33f87fb
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-08 14:14:23 -0600

    Adds predictions to user poro

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -2,12 +2,14 @@ class User
   attr_reader :first_name,
               :last_name,
               :email,
-              :id
+              :id,
+              :predictions
 
   def initialize(data)
     @first_name = data[:data][:attributes][:first_name]
     @last_name = data[:data][:attributes][:last_name]
     @email = data[:data][:attributes][:email]
     @id = data[:data][:id]
+    @predictions = data[:data][:attributes][:predictions]
   end
 end

fe119a3c0a2bf3a311288a6eca4b595f96e97968
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-08 14:14:03 -0600

    Adds update_user_prediction and delete_prediction to UserFacade

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,4 +1,12 @@
 class UserFacade
+  def update_user_prediction(data)
+    service.update_user_prediction(data)
+  end
+
+  def delete_prediction(user_id, id)
+    service.delete_prediction(user_id, id)
+  end
+
   def current_user(session_id)
     user = service.current_user(session_id)
     User.new(user)
